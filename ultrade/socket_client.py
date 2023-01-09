8b286188e4c7443b71ee03ab20f294821e96587d
EugeneLas <eugene.lasminski@yumasoft.com>
2023-01-09 20:15:05 +0300

    change socketIO client to asynchronous

--- ultrade/socket_client.py
+++ ultrade/socket_client.py
@@ -2,7 +2,7 @@ import socketio
 import time
 from typing import Callable, Optional, TypedDict, Dict
 
-socket: Optional[socketio.Client] = None
+socket: Optional[socketio.AsyncClient] = None
 socket_pool: Optional[dict[str, 'SubscribeOptions']] = {}
 
 
@@ -12,41 +12,42 @@ class SubscribeOptions(TypedDict):
     options: Dict[str, any]
 
 
-def subscribe(url: str, options: SubscribeOptions, callback: Callable[[str, list[any]], any]):
+async def subscribe(url: str, options: SubscribeOptions, callback: Callable[[str, list[any]], any]):
     handler_id = str(time.time_ns())
     global socket
 
     if not socket:
-        socket = socketio.Client()
+        socket = socketio.AsyncClient()
         add_event_listeners(socket, options, callback)
-        socket.connect(url)
+        await socket.connect(url)
+        await socket.wait()
 
-    socket.emit("subscribe", options)
+    await socket.emit("subscribe", options)
     socket_pool[handler_id] = options
 
     return handler_id
 
 
-def unsubscribe(handler_id: str):
+async def unsubscribe(handler_id: str):
     global socket
     try:
         options = socket_pool[handler_id]
-        socket.emit("unsubscribe", options)
+        await socket.emit("unsubscribe", options)
         del socket_pool[handler_id]
     finally:
         if len(socket_pool.keys()) == 0:
-            socket.disconnect()
+            await socket.disconnect()
             socket = None
 
 
 def add_event_listeners(socket: socketio.Client, options: SubscribeOptions, callback: Callable[[str, list[any]], any]):
     socket.on('*', callback)
 
-    def reconnect_handler():
-        socket.emit("subscribe", options)
+    async def reconnect_handler():
+        await socket.emit("subscribe", options)
     socket.on("reconnect", reconnect_handler)
 
-    def connect_handler():
+    async def connect_handler():
         print('ws connection established')
-        socket.emit("subscribe", options)
+        await socket.emit("subscribe", options)
     socket.on("connect", connect_handler)
