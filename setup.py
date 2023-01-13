7d03a9d5e821f42933d07d19c738f3cda0643dd5
EugeneLas <eugene.lasminski@yumasoft.com>
2023-01-13 13:55:48 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,7 @@
 import setuptools
-import codecs
-import os
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
 
 setuptools.setup(
-    name="ultrade2",
+    name="ultrade",
     version='0.1.1',
     description="",
     author="Ultrade",
