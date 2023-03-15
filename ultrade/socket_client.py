7a0fee7d278c6516a06f17fbc56322a2da1f7471
EugeneLas <eugene.lasminski@yumasoft.com>
2023-03-15 18:31:31 +0300

    remove socket instantiation from init func

--- ultrade/socket_client.py
+++ ultrade/socket_client.py
@@ -2,8 +2,6 @@ import socketio
 import time
 from typing import Callable, Optional, TypedDict, Dict, List
 
-socket_pool: Optional[Dict[str, 'SubscribeOptions']] = {}
-
 
 class SubscribeOptions(TypedDict):
     symbol: str
@@ -14,34 +12,47 @@ class SubscribeOptions(TypedDict):
 class SocketService():
     def __init__(self, url):
         self.subscribe_options = []
-        self.socket = socketio.AsyncClient(reconnection_delay_max=1000)
+        self.socket: Optional[socketio.AsyncClient] = None
         self.url = url
-        pass
+        self.socket_pool: Optional[Dict[str, 'SubscribeOptions']] = {}
+        self.isConnectionExist = False
 
     async def subscribe(self, options: SubscribeOptions, callback: Callable[[str, List[any]], any]):
+        if not self.isConnectionExist:
+            self.isConnectionExist = True
+            self.socket = socketio.AsyncClient(reconnection_delay_max=1000)
+            self.add_event_listeners()
+            await self.socket.connect(self.url, transports=["websocket"])
+            await self.socket.wait()
+
         handler_id = str(time.time_ns())
-        self.add_event_listeners(options, callback)
-        await self.socket.connect(self.url, transports=["websocket"])
-        await self.socket.wait()
 
         await self.socket.emit("subscribe", options)
-        socket_pool[handler_id] = options
+        self.socket_pool[handler_id] = options
 
         return handler_id
 
     async def unsubscribe(self, handler_id: str):
-        options = socket_pool[handler_id]
-        await self.socket.emit("unsubscribe", options)
-        del socket_pool[handler_id]
+        try:
+            options = self.socket_pool[handler_id]
+            await self.socket.emit("unsubscribe", options)
+            del self.socket_pool[handler_id]
+        finally:
+            if len(self.socket_pool.keys()) == 0:
+                await self.socket.disconnect()
+                self.socket = None
 
-    def add_event_listeners(self, options: SubscribeOptions, callback: Callable[[str, List[any]], any]):
-        self.socket.on('*', callback)
+    def add_event_listeners(self):
+        self.socket.on('*', self.callback_handler)
 
         async def reconnect_handler():
-            await self.socket.emit("subscribe", options)
+            await self.socket.emit("subscribe", self.subscribe_options)
         self.socket.on("reconnect", reconnect_handler)
 
         async def connect_handler():
             print('ws connection established')
-            await self.socket.emit("subscribe", options)
+            await self.socket.emit("subscribe", self.subscribe_options)
         self.socket.on("connect", connect_handler)
+
+    async def callback_handler():
+        pass
