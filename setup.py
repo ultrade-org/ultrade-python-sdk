4038e9a36f9d47103bf0656d00fe8d34418738fb
Eugene <eugene.lasminski@yumasoft.com>
2025-02-10 20:20:38 +0300

    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ required_packages = (this_directory / "requirements.txt").read_text().splitlines
 
 setuptools.setup(
     name="ultrade-sdk",
-    version="0.3.10",
+    version="0.3.11",
     license="MIT",
     description="This SDK provides interface that helps making trading operations within the Ultrade network easier",
     long_description=long_description,
